fcce4afb600926bd96f9699856ffdcac1bc8b7b8
Amin23 <alamin03rockz@gmail.com>
2025-12-18 20:01:41 +0530

    Initial commit

--- prisma.config.js
+++ prisma.config.js
@@ -1,7 +1,8 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+
 require("dotenv/config");
-exports.default = {
-    schema: "prisma/schema.prisma",
+
+module.exports = {
+  schema: "prisma/schema.prisma",
 };
 //# sourceMappingURL=prisma.config.js.map
